77c8e81943e5f315af4fcca0ad9af5f4d393c18f
IAN <choestro@gmail.com>
2024-09-06 14:56:44 +0900

    Create project

---
+++ src/main/java/com/junebay/plancard/PlancardApplication.java
@@ -0,0 +1,13 @@
+package com.junebay.plancard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlancardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlancardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junebay/plancard/PlancardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junebay.plancard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlancardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

7fda6fcb7ad55a2e6d4eca1dc821cdc2ef8d1fba
IAN <choestro@gmail.com>
2024-09-26 17:35:54 +0900

    city & coutry data refined and insert test finish

--- src/main/resources/Plancard.sql
+++ src/main/resources/Plancard.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `cards` (
-  `id` bigint PRIMARY KEY COMMENT '카드(지역) 고유번호',
+  `id` bigint AUTO_INCREMENT PRIMARY KEY COMMENT '카드(지역) 고유번호',
   `title` varchar(255) NOT NULL COMMENT '카드(지역)명',
-  `description` varchar(255) COMMENT '설명',
-  `city_id` varchar(255) NOT NULL COMMENT '도시 고유번호. 카드와 도시는 N:1 관계이다.',
+  `description` varchar(1500) COMMENT '설명',
+  `city_id` varchar(50) NOT NULL COMMENT '도시 고유번호. 카드와 도시는 N:1 관계이다.',
   `category_id` bigint COMMENT '카테고리 고유번호 (관광지, 유적지, ...)',
   `rating` tinyint COMMENT '별점(1점 단위)',
   `is_active` bool NOT NULL DEFAULT true COMMENT '사용 유무',
@@ -11,29 +11,29 @@ CREATE TABLE `cards` (
 ) COMMENT = '카드(지역) 정보 테이블. 국가:도시:카드(지역) 1:N:M 관계를 나타낸다.';
 
 CREATE TABLE `cities` (
-  `id` varchar(255) PRIMARY KEY COMMENT '도시 고유번호',
+  `id` varchar(50) PRIMARY KEY COMMENT '도시 고유번호',
   `name` varchar(255) NOT NULL COMMENT '도시명',
   `name_ascii` varchar(255) COMMENT '도시명 ASCII 버전',
-  `description` varchar(255) COMMENT '설명',
-  `latitude` decimal(9,6) COMMENT '위도(-90 ~ +90)',
-  `longitude` decimal(9,6) COMMENT '경도(-180 ~ +180)',
-  `country_id` varchar(255) COMMENT '국가 고유번호. 도시와 국가는 N:1 관계이다.',
+  `description` varchar(1500) COMMENT '설명',
+  `latitude` double COMMENT '위도(-90 ~ +90)',
+  `longitude` double COMMENT '경도(-180 ~ +180)',
+  `country_id` varchar(20) COMMENT '국가 고유번호. 도시와 국가는 N:1 관계이다.',
   `is_active` bool NOT NULL DEFAULT true COMMENT '사용 유무',
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
   `updated_date` datetime COMMENT '최종 수정일'
 ) COMMENT = '도시 정보 테이블. 도시 테이블은 국가 테이블과 N:1 관계이다.';
 
 CREATE TABLE `countries` (
-  `id` varchar(255) PRIMARY KEY COMMENT '국가 고유번호',
+  `id` varchar(50) PRIMARY KEY COMMENT '국가 고유번호',
   `name` varchar(255) NOT NULL COMMENT '국가명',
-  `description` varchar(255) COMMENT '설명',
+  `description` varchar(1500) COMMENT '설명',
   `is_active` bool NOT NULL DEFAULT true COMMENT '사용 유무',
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
   `updated_date` datetime COMMENT '최종 수정일'
 ) COMMENT = ' UNComtrade 에서 제공하는 국가 정보 테이블.';
 
 CREATE TABLE `themes` (
-  `id` bigint PRIMARY KEY COMMENT '테마 고유번호',
+  `id` bigint AUTO_INCREMENT PRIMARY KEY COMMENT '테마 고유번호',
   `name` varchar(255) NOT NULL COMMENT '테마명',
   `is_active` bool NOT NULL DEFAULT true COMMENT '사용 유무',
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
@@ -49,7 +49,7 @@ CREATE TABLE `card_theme` (
 ) COMMENT = '카드와 테마의 중간 테이블. 여러 장의 카드는 여러 가지 테마를 가질 수 있다. 카드와 테마는 N:M 관계이다.';
 
 CREATE TABLE `categories` (
-  `id` bigint PRIMARY KEY COMMENT '카테고리 고유번호',
+  `id` bigint AUTO_INCREMENT PRIMARY KEY COMMENT '카테고리 고유번호',
   `name` varchar(255) NOT NULL COMMENT '카테고리명',
   `is_active` bool NOT NULL DEFAULT true COMMENT '사용 유무',
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
@@ -57,12 +57,13 @@ CREATE TABLE `categories` (
 ) COMMENT = '하나의 카드(지역)에 하나만 할당되는 카테고리 정보';
 
 CREATE TABLE `card_images` (
-  `id` bigint PRIMARY KEY COMMENT '카드(지역) 이미지 고유번호',
+  `id` bigint AUTO_INCREMENT PRIMARY KEY COMMENT '카드(지역) 이미지 고유번호',
   `card_id` bigint NOT NULL COMMENT '카드(지역) 고유번호(FK)',
   `save_path` varchar(255) NOT NULL COMMENT '저장 경로',
   `file_name` varchar(255) NOT NULL COMMENT '파일명',
   `extension` varchar(255) NOT NULL COMMENT '확장자',
   `size` int COMMENT '파일 크기(byte)',
+  `alt` varchar(255) COMMENT 'ALT 텍스트',
   `is_main` bool NOT NULL COMMENT '메인사진 유무',
   `is_active` bool NOT NULL DEFAULT true COMMENT '사용 유무',
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
@@ -70,12 +71,13 @@ CREATE TABLE `card_images` (
 ) COMMENT = '카드(지역)에 할당되는 이미지. 카드 테이블과 카드 이미지 테이블은 1:N 관계이다.';
 
 CREATE TABLE `city_images` (
-  `id` bigint PRIMARY KEY COMMENT '도시 이미지 고유번호',
-  `city_id` varchar(255) NOT NULL COMMENT '도시 고유번호(FK)',
+  `id` bigint AUTO_INCREMENT PRIMARY KEY COMMENT '도시 이미지 고유번호',
+  `city_id` varchar(50) NOT NULL COMMENT '도시 고유번호(FK)',
   `save_path` varchar(255) NOT NULL COMMENT '저장 경로',
   `file_name` varchar(255) NOT NULL COMMENT '파일명',
   `extension` varchar(255) NOT NULL COMMENT '확장자',
   `size` int COMMENT '파일 크기(byte)',
+  `alt` varchar(255) COMMENT 'ALT 텍스트',
   `is_main` bool NOT NULL COMMENT '메인사진 유무',
   `is_active` bool NOT NULL DEFAULT true COMMENT '사용 유무',
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
@@ -83,12 +85,13 @@ CREATE TABLE `city_images` (
 ) COMMENT = '도시에 할당되는 이미지. 도시 테이블과 도시 이미지 테이블은 1:N 관계이다.';
 
 CREATE TABLE `country_images` (
-  `id` bigint PRIMARY KEY COMMENT '국가 이미지 고유번호',
-  `country_id` varchar(255) NOT NULL COMMENT '국가 고유번호(FK)',
+  `id` bigint AUTO_INCREMENT PRIMARY KEY COMMENT '국가 이미지 고유번호',
+  `country_id` varchar(50) NOT NULL COMMENT '국가 고유번호(FK)',
   `save_path` varchar(255) NOT NULL COMMENT '저장 경로',
   `file_name` varchar(255) NOT NULL COMMENT '파일명',
   `extension` varchar(255) NOT NULL COMMENT '확장자',
   `size` int COMMENT '파일 크기(byte)',
+  `alt` varchar(255) COMMENT 'ALT 텍스트',
   `is_main` bool NOT NULL COMMENT '메인사진 유무',
   `is_active` bool NOT NULL DEFAULT true COMMENT '사용 유무',
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
@@ -96,7 +99,7 @@ CREATE TABLE `country_images` (
 ) COMMENT = '국가에 할당되는 이미지. 국가 테이블과 국가 이미지 테이블은 1:N 관계이다.';
 
 CREATE TABLE `card_map_links` (
-  `id` bigint PRIMARY KEY COMMENT '카드 맵 링크 고유번호',
+  `id` bigint AUTO_INCREMENT PRIMARY KEY COMMENT '카드 맵 링크 고유번호',
   `card_id` bigint NOT NULL COMMENT '카드 고유번호(FK)',
   `platform` varchar(255) NOT NULL COMMENT '지도 플랫폼 구분 (예: google_maps, naver_maps 등)',
   `map_link` varchar(255) NOT NULL COMMENT '지도 링크(URL)',
@@ -106,17 +109,17 @@ CREATE TABLE `card_map_links` (
 ) COMMENT = '카드(지역) 위치정보 링크 정보 테이블';
 
 CREATE TABLE `my_cards` (
-  `id` bigint PRIMARY KEY COMMENT '내 카드 고유번호',
+  `id` bigint AUTO_INCREMENT PRIMARY KEY COMMENT '내 카드 고유번호',
   `card_id` bigint NOT NULL COMMENT '카드 고유번호(FK)',
   `user_id` bigint NOT NULL COMMENT '유저 고유번호(FK)',
-  `memo` varchar(255) COMMENT '메모',
+  `memo` varchar(1500) COMMENT '메모',
   `is_active` bool NOT NULL DEFAULT true COMMENT '사용 유무',
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '내 카드 얻은날짜(getdate)',
   `updated_date` datetime COMMENT '최종 수정일'
 ) COMMENT = '내 카드 정보. 특정 지역에서 여행 인증을 하면 카드(지역)의 커스텀 카드를 획득할 수 있다.';
 
 CREATE TABLE `my_card_tags` (
-  `id` bigint NOT NULL COMMENT '내 카드 태그 고유번호',
+  `id` bigint AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT '내 카드 태그 고유번호',
   `my_card_id` bigint NOT NULL COMMENT '내 카드 고유번호(FK)',
   `name` varchar(255) NOT NULL COMMENT '태그명',
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
@@ -124,12 +127,13 @@ CREATE TABLE `my_card_tags` (
 ) COMMENT = '내 카드의 태그정보 테이블. 5개까지 추가할 수 있는 사용자 정의 태그 정보이다.';
 
 CREATE TABLE `my_card_images` (
-  `id` bigint PRIMARY KEY COMMENT '내 카드 이미지 고유번호',
+  `id` bigint AUTO_INCREMENT PRIMARY KEY COMMENT '내 카드 이미지 고유번호',
   `my_card_id` bigint NOT NULL COMMENT '내 카드 고유번호(FK)',
   `save_path` varchar(255) NOT NULL COMMENT '저장 경로',
   `file_name` varchar(255) NOT NULL COMMENT '파일명',
   `extension` varchar(255) NOT NULL COMMENT '확장자',
   `size` int COMMENT '파일 크기(byte)',
+  `alt` varchar(255) COMMENT 'ALT 텍스트',
   `is_main` bool NOT NULL COMMENT '메인사진 유무',
   `is_active` bool NOT NULL DEFAULT true COMMENT '사용 유무',
   `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
@@ -137,7 +141,7 @@ CREATE TABLE `my_card_images` (
 ) COMMENT = '내 카드 이미지정보 테이블.';
 
 CREATE TABLE `users` (
-  `id` bigint PRIMARY KEY COMMENT '사용자 고유번호',
+  `id` bigint AUTO_INCREMENT PRIMARY KEY COMMENT '사용자 고유번호',
   `email` varchar(255) UNIQUE NOT NULL COMMENT '이메일',
   `password` varchar(255) COMMENT '암호화된 비밀번호',
   `provider` varchar(255) COMMENT '소셜로그인 제공자',
@@ -157,9 +161,9 @@ CREATE TABLE `card_scraps` (
 ) COMMENT = '사용자가 스크랩한 카드(지역) 정보 테이블';
 
 CREATE TABLE `plans` (
-  `id` bigint PRIMARY KEY COMMENT '여행계획 고유번호',
+  `id` bigint AUTO_INCREMENT PRIMARY KEY COMMENT '여행계획 고유번호',
   `title` varchar(255) COMMENT '여행계획명',
-  `memo` varchar(255) COMMENT '메모',
+  `memo` varchar(1500) COMMENT '메모',
   `start_date` date COMMENT '시작일',
   `end_date` date COMMENT '종료일',
   `is_explore` bool NOT NULL DEFAULT true COMMENT '플랜 탐험 등록유무',
@@ -170,7 +174,7 @@ CREATE TABLE `plans` (
 ) COMMENT = '사용자 정의 여행 계획정보 테이블. 여행일자, 여행일자별 카드, 여행일자별 브릿지로 구성';
 
 CREATE TABLE `plan_days` (
-  `id` bigint PRIMARY KEY COMMENT '여행계획 - 일자 고유번호',
+  `id` bigint AUTO_INCREMENT PRIMARY KEY COMMENT '여행계획 - 일자 고유번호',
   `plan_id` bigint NOT NULL COMMENT '여행계획 고유번호(FK)',
   `title` varchar(255) COMMENT '여행계획명',
   `travel_date` date NOT NULL COMMENT '실제 여행일자',
@@ -179,7 +183,7 @@ CREATE TABLE `plan_days` (
 ) COMMENT = '사용자 정의 여행 계획정보 - 일별 정보 테이블. 카드와 브릿지로 구성';
 
 CREATE TABLE `plan_day_cards` (
-  `id` bigint PRIMARY KEY COMMENT '여행계획 - 카드(지역) 고유번호',
+  `id` bigint AUTO_INCREMENT PRIMARY KEY COMMENT '여행계획 - 카드(지역) 고유번호',
   `plan_day_id` bigint COMMENT '여행계획 - 일자 고유번호(FK)',
   `seq` int COMMENT '해당 SEQ는 카드순서 변경에 의해 언제든지 변동가능',
   `card_id` bigint COMMENT '카드(지역) 고유번호(FK)',
@@ -189,11 +193,11 @@ CREATE TABLE `plan_day_cards` (
 CREATE TABLE `plan_day_bridges` (
   `plan_day_card_id` bigint COMMENT '여행계획 - 일자별 카드 고유번호',
   `plan_day_card_seq` int COMMENT '여행계획 - 일자별 카드 순서',
-  `memo` varchar(255) COMMENT '메모'
+  `memo` varchar(1500) COMMENT '메모'
 ) COMMENT = '사용자 정의 여행 계획 - 일자별 카드별 브릿지 정보 테이블.';
 
 CREATE TABLE `plan_scraps` (
-  `id` bigint PRIMARY KEY COMMENT '여행계획 스크랩 고유번호',
+  `id` bigint AUTO_INCREMENT PRIMARY KEY COMMENT '여행계획 스크랩 고유번호',
   `plan_id` bigint COMMENT '스크랩한 플랜 고유번호(FK)',
   `user_id` bigint COMMENT '스크랩한 사용자 고유번호(FK)'
 );

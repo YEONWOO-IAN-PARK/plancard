2f86278968f9655da888811d13bf657d57b0b0ef
IAN <choestro@gmail.com>
2025-05-08 18:50:36 +0900

    my card memo, my card tag done. please check README.md

---
+++ src/main/java/com/junebay/plancard/card/dto/CardScrapDTO.java
@@ -0,0 +1,6 @@
+package com.junebay.plancard.card.dto;/**
+ * @author         : IAN
+ * @date           : 2025-05-08
+ * @description    :
+ */public class CardScrapDTO {
+}
---
+++ src/main/java/com/junebay/plancard/common/exception/NoContentException.java
@@ -0,0 +1,6 @@
+package com.junebay.plancard.common.exception;/**
+ * @author         : IAN
+ * @date           : 2025-04-29
+ * @description    :
+ */public class NoContentException {
+}
